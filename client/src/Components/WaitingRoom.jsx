47f212cf2e00a0c0d87baa9d11bd0f88c09beb90
batubat <batubat@thank-u-next.home>
2024-05-05 20:26:30 +0200

    better titles and names 2

--- client/src/Components/WaitingRoom.jsx
+++ client/src/Components/WaitingRoom.jsx
@@ -4,8 +4,11 @@ function WaitingRoom() {
     
     return (
         <div >
-            <h1>Waiting Room</h1>
-            <p>Veuillez patienter d'être accepté</p>
+            <h1>En Attente de Validation</h1>
+            <p>Nous sommes ravis de vous voir ici. Votre demande d'inscription est actuellement en cours de traitement par notre équipe administrative. Nous comprenons que l'attente peut parfois être frustrante, mais soyez assuré que nous faisons de notre mieux pour traiter votre demande dans les plus brefs délais.</p>
+            <p>Nos administrateurs vérifient chaque demande avec soin afin de garantir la meilleure expérience possible pour tous nos utilisateurs. Nous vous remercions de votre patience pendant ce processus.</p>
+            <p>Merci pour votre compréhension.</p>
+            <p>L'équipe de Organiz'asso</p>
         </div>
     );
 }
