581968634a6e6f9fdff2206c289dcfe0fd999a31
batubat <batubat@thank-u-next.home>
2024-05-08 17:07:04 +0200

    ajout logo

--- client/src/Components/Auth/Start.jsx
+++ client/src/Components/Auth/Start.jsx
@@ -14,7 +14,7 @@ function Start() {
 
   return (
     <div className="premier">
-      <img src="https://png.pngtree.com/png-clipart/20211009/original/pngtree-aesthetic-pink-flower-logo-with-outline-png-image_6846146.png" alt="logo"></img>
+      <img src="https://png.pngtree.com/png-clipart/20230325/original/pngtree-aesthetic-lavender-png-image_9003211.png" alt="logo"></img>
       <button onClick={goToLogin}>Connexion</button>
       <button onClick={goToSignUp}>Inscription</button>
     </div>
